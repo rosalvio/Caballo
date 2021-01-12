f393adf0038e018cd43009402085180a2a8e85c3
Netzuel <56087693+Netzuel@users.noreply.github.com>
2021-01-12 16:34:31 +0100

    He añadido para plotear y guardar la CSF

--- caballo.py
+++ caballo.py
@@ -2,6 +2,14 @@ import cv2
 import glob
 import numpy as np
 import random
+from datetime import datetime
+
+freq_x = [1.5, 3, 6, 12, 18]
+contrastes =[[0.8,0.6,0.3,0.15,0.075,0.0375,0.009375,0.01875,0.00446875],
+[0.6,0.3,0.15,0.075,0.0375,0.01875,0.009375,0.0046875],
+[0.6,0.3,0.15,0.075,0.0375,0.01875,0.009375,0.00703125,0.0046875],
+[0.8,0.6,0.3,0.15,0.075,0.0375,0.01875,0.009375],
+[1,0.8,0.6,0.3,0.15,0.075,0.0375,0.01875]]
 
 
 colorGris = 159
@@ -10,17 +18,17 @@ base_imagenes = "images/"
 pruebas = ["1.png", "2.png"]
 imgs_1_5 = ["1.5/1,5-0,8.png","1.5/1,5-0,6.png","1.5/1,5-0,3.png","1.5/1,5-0,15.png","1.5/1,5-0,075.png","1.5/1,5-0,0375.png","1.5/1,5-0,009375.png","1.5/1.5-0,01875.png","1.5/1,5-0,00446875.png"]
 imgs_3 = ["3/3-0.6.png","3/3-0.3.png","3/3-0.15.png","3/3-0.075.png","3/3-0.0375.png","3/3-0.01875.png","3/3-0.009375.png","3/3-0.0046875.png"]
-imgs_6 = ["6/6-0.6.png","6/6-0.3.png","6/6-0.15.png","6/6-0.075.png","6/6-0.0375.png","6/6-0.01875.png","6/6-0.009375.png","6/6-0.0046875.png","6/6-0.00703125.png"]
+imgs_6 = ["6/6-0.6.png","6/6-0.3.png","6/6-0.15.png","6/6-0.075.png","6/6-0.0375.png","6/6-0.01875.png","6/6-0.009375.png","6/6-0.00703125.png","6/6-0.0046875.png"]
 imgs_12 = ["12/12-0.8.png","12/12-0.6.png","12/12-0.3.png","12/12-0.15.png","12/12-0.075.png","12/12-0.0375.png","12/12-0.01875.png","12/12-0.009375.png"]
 imgs_18 = ["18/18-1.png","18/18-0.8.png","18/18-0.6.png","18/18-0.3.png","18/18-0.15.png","18/18-0.075.png","18/18-0.0375.png","18/18-0.01875.png"]
 caballo = cv2.imread(f'{base_imagenes}mascara2.png', 0)
-caballo = cv2.resize(caballo, (500,500))
+caballo = cv2.resize(caballo, (500,500), interpolation=cv2.INTER_NEAREST)
 gris = cv2.imread(f'{base_imagenes}gris2.png', 0)
 gris = cv2.resize(caballo, (500,500))
 blanco = cv2.imread(f'{base_imagenes}blanco.png', 0)
-blanco = cv2.resize(blanco, (500,500))
+blanco = cv2.resize(blanco, (500,500), interpolation=cv2.INTER_NEAREST)
 grisperodeverdadnomas = cv2.imread(f'{base_imagenes}todogris2.png', 0)
-grisperodeverdadnomas = cv2.resize(grisperodeverdadnomas, (500,500))
+grisperodeverdadnomas = cv2.resize(grisperodeverdadnomas, (500,500), interpolation=cv2.INTER_NEAREST)
 
 
 def imagenes(frecuencia):
@@ -29,7 +37,7 @@ def imagenes(frecuencia):
     """
     im2, im3, im4 = grisperodeverdadnomas,grisperodeverdadnomas,grisperodeverdadnomas
     frecuencia = cv2.imread(frecuencia, 0)
-    frecuencia = cv2.resize(frecuencia, (500,500))
+    frecuencia = cv2.resize(frecuencia, (500,500), interpolation=cv2.INTER_NEAREST)
     masked_horse = cv2.bitwise_and(frecuencia, gris)
     masked_horse = np.where(masked_horse==0, colorGris, masked_horse)
     masked_horse = cv2.bitwise_and(masked_horse, blanco)
@@ -68,56 +76,101 @@ def fadeIn (img1, img2, len=10):
     for IN in range(0,len):
         fadein = IN/float(len)
         dst = cv2.addWeighted( img1, 1-fadein, img2, fadein, 0)
-        cv2.imshow('Res', dst)
+        dst2 = cv2.resize(dst, (600,600), interpolation=cv2.INTER_NEAREST)
+        cv2.imshow('Res', dst2)
         cv2.waitKey(1)
+
+indices = []
 
 if __name__ == "__main__":
     nombre = input("Introduzca el nombre del paciente: ")
     print(nombre)
     ant = ''
+    j = 0
     for frecuencia in imgs_1_5:
+        print("Estamos en 1.5")
+        print(frecuencia)
         freq = f'{base_imagenes}{frecuencia}'
         imgs, cebra = imagenes(freq)
         barajadas, correcta = baraja(imgs, cebra)
         acierto = parpadeo(barajadas, correcta)
-        acierto = False
+        j = j + 1
         if not acierto:
+            print("Fallo")
             print(ant)
             break
         ant = frecuencia
+    indices.append(j-2)
+    j = 0
     for frecuencia in imgs_3:
+        print("Estamos en 3")
+        print(frecuencia)
         freq = f'{base_imagenes}{frecuencia}'
         imgs, cebra = imagenes(freq)
         barajadas, correcta = baraja(imgs, cebra)
         acierto = parpadeo(barajadas, correcta)
+        j = j + 1
         if not acierto:
+            print("Fallo")
             print(ant)
             break
         ant = frecuencia
+    indices.append(j-2)
+    j = 0
     for frecuencia in imgs_6:
+        print("Estamos en 6")
+        print(frecuencia)
         freq = f'{base_imagenes}{frecuencia}'
         imgs, cebra = imagenes(freq)
         barajadas, correcta = baraja(imgs, cebra)
         acierto = parpadeo(barajadas, correcta)
+        j = j + 1
         if not acierto:
+            print("Fallo")
             print(ant)
             break
         ant = frecuencia
+    indices.append(j-2)
+    j = 0
     for frecuencia in imgs_12:
+        print("Estamos en 12")
+        print(frecuencia)
         freq = f'{base_imagenes}{frecuencia}'
         imgs, cebra = imagenes(freq)
         barajadas, correcta = baraja(imgs, cebra)
         acierto = parpadeo(barajadas, correcta)
+        j = j + 1
         if not acierto:
+            print("Fallo")
             print(ant)
             break
         ant = frecuencia
+    indices.append(j-2)
+    j = 0
     for frecuencia in imgs_18:
+        print("Estamos en 18")
+        print(frecuencia)
         freq = f'{base_imagenes}{frecuencia}'
         imgs, cebra = imagenes(freq)
         barajadas, correcta = baraja(imgs, cebra)
         acierto = parpadeo(barajadas, correcta)
+        j = j + 1
         if not acierto:
+            print("Fallo")
             print(ant)
             break
         ant = frecuencia
+    indices.append(j-2)
+    umbrales = [contrastes[i][indices[i]] if indices[i] != -1 else 1 for i in range(len(contrastes))]
+    print("Umbrales: ", umbrales)
+
+    import matplotlib.pyplot as plt
+    y = 1/np.array(umbrales)
+    x = freq_x
+    plt.plot(x, y, "o--", color = "blue")
+    plt.yscale(value = "log")
+    plt.grid()
+    plt.xticks(freq_x, freq_x)
+    plt.xlabel("Frecuencia espacial (cpg)")
+    plt.ylabel("S (dB)")
+    plt.savefig(nombre + "_" + datetime.now().strftime('%m-%d-%Y') + ".png")
