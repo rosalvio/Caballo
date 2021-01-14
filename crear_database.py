dbdba286dcc6af4d0bf809ea83e1501ac95a8163
Netzuel <56087693+Netzuel@users.noreply.github.com>
2021-01-14 20:33:07 +0100

    Add files via upload

--- crear_database.py
+++ crear_database.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from datetime import datetime
 
-test_row = {"Nombre":"Nombre", "Apellidos": "Apellidos", "Fecha":"Fecha","1,5":1, "3":1, "6":1, "12":1, "18":1}
+test_row = {"Nombre":"Nombre", "Apellidos": "Apellidos", "Fecha":datetime.now().strftime('%Y-%m-%d'),"1,5":1, "3":1, "6":1, "12":1, "18":1}
 database = pd.DataFrame(columns = ["Nombre", "Apellidos", "Fecha", "1,5", "3", "6", "12", "18"])
 database = database.append(test_row, ignore_index = True)
 database.to_csv("database.csv", index = False)
